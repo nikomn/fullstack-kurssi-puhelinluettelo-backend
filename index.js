2e2b2e0d47ef9e0512b9c75bbe5d83edd60ebd7b
Niko Nieminen <niko.m.nieminen@helsinki.fi>
2020-08-09 11:22:30 +0300

    puhelinluettelon backend step4

--- index.js
+++ index.js
@@ -49,3 +49,10 @@ app.get('/info', (req, res) => {
       res.status(404).end()
     }
   })
+
+  app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(person => person.id !== id)
+  
+    res.status(204).end()
+  })

1dc410664fc3238065c9a369da87ea0edf6e010d
Niko Nieminen <niko.m.nieminen@helsinki.fi>
2020-08-09 11:16:58 +0300

    puhelinluettelon backend step3

--- index.js
+++ index.js
@@ -38,3 +38,14 @@ app.get('/info', (req, res) => {
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
   })
+
+  app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+    
+    if (person) {
+      res.json(person)
+    } else {
+      res.status(404).end()
+    }
+  })

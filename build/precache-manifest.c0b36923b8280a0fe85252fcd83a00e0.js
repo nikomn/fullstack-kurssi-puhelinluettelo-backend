7a20f89b7e19cf3b9f8a4673e3304e6e5d0931db
Niko Nieminen <niko.m.nieminen@helsinki.fi>
2020-08-10 12:22:44 +0300

    puhelinluettelon backend step9

---
+++ build/precache-manifest.c0b36923b8280a0fe85252fcd83a00e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5af46bcd0f00e0f978229151d186af60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6396add319b1d2053087",
+    "url": "/static/css/main.65f28afd.chunk.css"
+  },
+  {
+    "revision": "77ed0c26fb7a102b89fb",
+    "url": "/static/js/2.936a527d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.936a527d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6396add319b1d2053087",
+    "url": "/static/js/main.fb965e1e.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);

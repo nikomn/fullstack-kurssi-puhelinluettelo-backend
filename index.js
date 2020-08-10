588836251ba9c5fe19a619d027633e6145edb9be
Niko Nieminen <niko.m.nieminen@helsinki.fi>
2020-08-10 12:29:11 +0300

    puhelinluettelon backend step10

--- index.js
+++ index.js
@@ -54,74 +54,71 @@ app.get('/info', (req, res) => {
     res.send("Phonebook has info for " + persons.length + " people<p>" + new Date() + "</p>")
     //res.send('<h1>Hello World!</h1>')
     })
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
   
-  const PORT = 3001
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-  })
-
-  app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const person = persons.find(person => person.id === id)
-    
-    if (person) {
-      res.json(person)
-    } else {
-      res.status(404).end()
-    }
-  })
-
-  const generateId = () => {
-    const id = Math.floor(Math.random() * (999999 - 1000) + 1000)
-    console.log(`creted new id: ${id}`)
-    const test = persons.filter(person => person.id === id)
-    if (test.length > 0) {
-        console.log(`${id} already in use..`)
-    }
-    return id
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
   }
-  
-  app.post('/api/persons', (req, res) => {
-    const body = req.body
-  
-    if (!body.name) {
+})
+
+const generateId = () => {
+  const id = Math.floor(Math.random() * (999999 - 1000) + 1000)
+  console.log(`creted new id: ${id}`)
+  const test = persons.filter(person => person.id === id)
+  if (test.length > 0) {
+      console.log(`${id} already in use..`)
+  }
+  return id
+}
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+
+  if (!body.name) {
+    return res.status(400).json({ 
+      error: 'name missing' 
+    })
+  }
+
+  if (!body.number) {
       return res.status(400).json({ 
-        error: 'name missing' 
+        error: 'number missing' 
       })
     }
 
-    if (!body.number) {
-        return res.status(400).json({ 
-          error: 'number missing' 
+  const personTest = persons.find(person => person.name === body.name)
+
+  if (personTest) {
+      return res.status(400).json({ 
+          error: 'name already in database! Name must be unique' 
         })
-      }
+  }
 
-    const personTest = persons.find(person => person.name === body.name)
-
-    if (personTest) {
-        return res.status(400).json({ 
-            error: 'name already in database! Name must be unique' 
-          })
-    }
-  
-    const person = {
-      name: body.name,
-      number: body.number,
-      id: generateId()
-    }
-
-  
-    persons = persons.concat(person)
-  
-    res.json(person)
-  })
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId()
+  }
 
 
+  persons = persons.concat(person)
 
+  res.json(person)
+})
 
-  app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(person => person.id !== id)
-  
-    res.status(204).end()
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  res.status(204).end()
+})
+
+const PORT = process.env.PORT || 3001
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
   })

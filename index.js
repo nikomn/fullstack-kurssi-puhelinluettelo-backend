87f8a71124747980735e70ca78e370a1e8b8fbda
Niko Nieminen <niko.m.nieminen@helsinki.fi>
2020-08-11 14:04:42 +0300

    puhelinluettelo ja tietokanta, step3 ja step4

--- index.js
+++ index.js
@@ -63,11 +63,17 @@ app.get('/info', (req, res) => {
     //res.send('<h1>Hello World!</h1>')
     })
 
-app.get('/api/persons/:id', (req, res) => {
-    Person.findById(req.params.id).then(person => {
-      res.json(person)
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id)
+    .then(person => {
+      if (person) {
+        res.json(person)
+      } else {
+        res.status(404).end()
+      }
     })
-  })
+    .catch(error => next(error))
+})
 
 const generateId = () => {
   const id = Math.floor(Math.random() * (999999 - 1000) + 1000)
@@ -99,12 +105,31 @@ app.post('/api/persons', (req, res) => {
   })
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  persons = persons.filter(person => person.id !== id)
-
-  res.status(204).end()
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id)
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
 })
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 //const PORT = process.env.PORT || 3001
 const PORT = process.env.PORT
